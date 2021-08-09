87f99b97531ffbb514529612eee7b396120018b8
Jorge Moreira <jorgemoreira@jorges-mbp.home>
2021-08-09 01:00:23 +0100

    NLW6 - Added features from video 4

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,15 @@
-import {ButtonHTMLAttributes} from 'react'
+import { ButtonHTMLAttributes } from 'react'
 
 import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    IsOutlined?: boolean
+};
 
-export function Button(props: ButtonProps){
-    return(
-        <button className="button"{...props}/>
+export function Button({ IsOutlined = false, ...props }: ButtonProps) {
+    return (
+        <button 
+        className={`button ${IsOutlined ? 'outlined' : ''}`}
+        {...props} />
     )
 }
